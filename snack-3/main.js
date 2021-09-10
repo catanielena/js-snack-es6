6b917846c7e649fc61217a20137aeeb76d4b52cc
catanielena <catani.elena1@gmail.com>
2021-09-10 15:41:51 +0200

    Add: snack-3

---
+++ snack-3/main.js
@@ -0,0 +1,23 @@
+// Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+// Usiamo i nuovi metodi degli array foreach o filter.
+// *
+// * For each
+// *
+function splitArr(arr, a, b) {
+    const result = [];
+    arr.forEach((elm, i) => {
+        if (i>=a && i<=b && !(isNaN(a) || isNaN(b) || a> b || b>arr.length)) {
+            result.push(elm);
+        }    
+    });
+    return result;
+};
+// *
+// * Filter
+// *
+const splitArr = (arr, a, b) => arr.filter((elm, i) => i>=a && i<=b && !(isNaN(a) || isNaN(b) || a> b || b>arr.length));
+// *
+// *
+let arr = ["a", "b", "c", "d", "e", "f", "g"];
+console.log(splitArr(arr, 2, 5));
